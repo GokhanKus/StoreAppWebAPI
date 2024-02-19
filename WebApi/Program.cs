59d7634b06f08a7b48764ad9a5d22c318f520411
GokhanKus <gkus1998@hotmail.com>
2024-02-19 16:01:03 +0300

    BooksController.cs, Crud islemleri http get, post, put, delete, patch

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -14,7 +14,7 @@ namespace WebApi
 			builder.Services.AddDbContext<RepositoryContext>(options =>
 				options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
 
-			builder.Services.AddControllers();
+			builder.Services.AddControllers().AddNewtonsoftJson();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
