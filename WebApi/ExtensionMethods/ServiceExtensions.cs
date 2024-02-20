29e524385b50647e1e251381733fc34f0c7135fc
GokhanKus <gkus1998@hotmail.com>
2024-02-20 15:47:38 +0300

    ServiceExtension.cs

---
+++ WebApi/ExtensionMethods/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Context;
+using System.Runtime.CompilerServices;
+
+namespace WebApi.ExtensionMethods
+{
+	public static class ServiceExtensions
+	{
+		public static void SqlConfiguration(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddDbContext<RepositoryContext>(options =>
+				options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+		}
+	}
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Repositories.Context;
+using WebApi.ExtensionMethods;
 
 namespace WebApi
 {
@@ -11,8 +12,7 @@ namespace WebApi
 
 			//Dbcontexte ihtiyacim oldugunda reposcontext ver, di ioc
 			//injection 3 kýsma ayrilir once kayit islemi sonra cozme islemi sonrada yasam suresi (Register, Resolve, Dispose)
-			builder.Services.AddDbContext<RepositoryContext>(options =>
-				options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
+			builder.Services.SqlConfiguration(builder.Configuration);
 
 			builder.Services.AddControllers().AddNewtonsoftJson();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
