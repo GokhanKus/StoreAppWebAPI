abf52bd58e5afa84b90f7819487649658a63e196
GokhanKus <gkus1998@hotmail.com>
2024-02-20 12:21:30 +0300

    Repositories Layer, IRepositoryBase.cs

---
+++ Repositories/Contracts/IRepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Contracts
+{
+	public interface IRepositoryBase<TEntity>
+	{
+		IQueryable<TEntity> FindAll(bool trackChanges);
+		IQueryable<TEntity> FindByCondition(Expression <Func<TEntity, bool>> expression, bool trackChanges); //burası sorgu sonucu bir varlık koleksiyonu döndürür, tek bir entity dondurmek istiyorsak IQueryable silersin
+		void Create(TEntity entity); 
+		void Update(TEntity entity); 
+		void Delete(TEntity entity); 
+	}
+}
+//ef coreda datayı ve degisiklikleri izleme ozelligi vardir(track changes) degisikleri izler ve SaveChanges() dedigimizde degisiklikleri yansitir
+//bazen degisiklikleri izlememek bize performans kazandirir her zaman izlemeye gerek olmaz
