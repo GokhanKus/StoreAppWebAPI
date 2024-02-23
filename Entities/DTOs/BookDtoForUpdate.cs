78e944fb0876a82bdd748336a3e0cf1c76b2ee76
GokhanKus <gkus1998@hotmail.com>
2024-02-23 17:41:03 +0300

    BookDtoForUpdate.cs, dto, record, init kavramlari

---
+++ Entities/DTOs/BookDtoForUpdate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+	public record BookDtoForUpdate(int Id, string Title, decimal Price);
+	//public record BookDtoForUpdate (bu 2 kullanim ayni, ikiside init;)
+	//{
+	//	public int Id { get; init; }
+	//	public string Title { get; init; }
+	//	public decimal Price { get; init; }
+	//}
+}
+/*
+Biz bir DTO(data transfer object)'dan bahsediyorsak bu 
+readonly (immutable) olmalı
+
+record ifadesi;
+LINQ destegi vardir yani uzerinde sorgular yazılabilir
+Referans tiptir, class ile ayni ozellikleri tasir
+Ctor tanimliyor gibi DTO tanimlamamiza izin verir?
+
+//set; yerine init(initialize); yaparak immutable(readonly) ozelligi kazandirmis oluyoruz yani
+tanimlandigi yerde degeri verilebilir sadece o zaman set edilebilir, o asamadan sonra o propun degisme ihtimali yoktur
+ */
