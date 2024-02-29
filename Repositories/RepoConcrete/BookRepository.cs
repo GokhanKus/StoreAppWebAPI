8c900e68646085267d74367db3ed9a01111d5981
GokhanKus <gkus1998@hotmail.com>
2024-02-29 13:15:22 +0300

    PagedList'in BookRepository.cs'te implementasyonu

--- Repositories/RepoConcrete/BookRepository.cs
+++ Repositories/RepoConcrete/BookRepository.cs
@@ -21,12 +21,13 @@ namespace Repositories.RepoConcrete
 		public void CreateOneBook(Book book) => Create(book);
 		public void UpdateOneBook(Book book) => Update(book);
 		public void DeleteOneBook(Book book) => Delete(book);
-		public async Task<IEnumerable<Book>> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
+		public async Task<PagedList<Book>> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
 		{
-			return await FindAll(trackChanges)
+			var books = await FindAll(trackChanges)
 				.OrderBy(i => i.Id)
-				.Paginate(bookParameters.PageNumber, bookParameters.PageSize)
 				.ToListAsync();
+
+			return PagedList<Book>.ToPagedList(books, bookParameters.PageNumber, bookParameters.PageSize);
 		}
 		public async Task<Book> GetOneBookByIdAsync(int id, bool trackChanges)
 		{
