81ba545d8a8c7a6ae145d438c8c101c7698c0595
GokhanKus <gkus1998@hotmail.com>
2024-02-29 17:30:09 +0300

    BadRequestException.cs ve PriceOutOfRangeException.cs

---
+++ Entities/Exceptions/BadRequestException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Exceptions
+{
+	//BadRequest abstract oldugu icin newlenemez (newlenemedigi icin ctorun public olmasinin anlami yok, cunku ctor o class newlendigi zaman calisir ama abstract oldugu icin newlenemiyor)
+	public abstract class BadRequestException : Exception
+	{
+		//eger BadRequest classi kalitim ile alinacaksa o classta ctor icinde message parametresi verilmek zorunda
+		//ctor(string message) : base(message)
+		protected BadRequestException(string message) : base(message)
+		{
+
+		}
+	}
+}
---
+++ Entities/Exceptions/PriceOutOfRangeBadRequestException.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities.Exceptions
+{
+	//PriceOutOfRangeBadRequest'u sealed oldugu kalıtım ile devralamazsin, yani bir bakima bu classin final version oldugunu soyluyoruz
+	public sealed class PriceOutOfRangeBadRequestException : BadRequestException
+	{
+		public PriceOutOfRangeBadRequestException() : base("max price must be less than 5000 and greater than 10")
+		{
+
+		}
+	}
+}
