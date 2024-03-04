fd6d033a96dd496067b7e690954f2d14137ff058
GokhanKus <gkus1998@hotmail.com>
2024-03-04 13:58:16 +0300

    LinkResponse.cs ve LinkCollectionWrapper.cs

---
+++ Entities/LinkModels/LinkCollectionWrapper.cs
@@ -0,0 +1,23 @@
+﻿namespace Entities.LinkModels
+{
+	public class LinkCollectionWrapper<T> : LinkResourceBase
+	{
+		public List<T> Value { get; set; } = new List<T>();
+
+		public LinkCollectionWrapper(List<T> value)
+		{
+			Value = value;
+		}
+		public LinkCollectionWrapper()
+		{
+
+		}
+	}
+}
+/*
+LinkCollectionWrapper<T> Sınıfı:
+LinkResourceBase sınıfını miras alıyor, böylece bu sınıfın içindeki Links özelliği, LinkResourceBase sınıfındaki gibi bağlantıları temsil ediyor.
+Value özelliği, bu koleksiyonun içindeki nesnelerin listesini tutar.
+LinkCollectionWrapper sınıfı, belirli bir kaynağın üzerinde bulunan bağlantıları ve bu kaynağın koleksiyonunu içeren bir yapı sağlar. 
+Bu genellikle koleksiyonları alırken veya döndürürken bağlantıları da beraberinde göndermek için kullanılır.
+*/
---
+++ Entities/LinkModels/LinkResponse.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.LinkModels
+{
+	public class LinkResponse
+	{
+		public bool HasLinks { get; set; }
+		public List<Entity> ShapedEntities { get; set; }
+		public LinkCollectionWrapper<Entity> LinkedEntities { get; set; }
+		public LinkResponse()
+		{
+			ShapedEntities = new List<Entity>();
+			LinkedEntities = new LinkCollectionWrapper<Entity>();
+		}
+	}
+}
+/*
+LinkResponse Sınıfı:
+HasLinks özelliği, yanıtın içinde bağlantıların olup olmadığını belirtir.
+ShapedEntities özelliği, yanıt içinde dönülen ana veri varsa bu verilerin listesini tutar.
+LinkedEntities özelliği, LinkCollectionWrapper<Entity> türünden bağlantıları ve bunlarla ilişkilendirilmiş varlık koleksiyonunu içerir.
+Bu, yanıtın içinde dönülen ana verilerin yanında ilişkili bağlantıları da taşımak için kullanılabilir.
+*/
