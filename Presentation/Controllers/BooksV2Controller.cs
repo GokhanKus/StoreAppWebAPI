39f99c4e530c76250bfc7f1c22131c4433993b51
GokhanKus <gkus1998@hotmail.com>
2024-03-08 19:27:49 +0300

    Header ile Api versiyonlama

--- Presentation/Controllers/BooksV2Controller.cs
+++ Presentation/Controllers/BooksV2Controller.cs
@@ -11,7 +11,8 @@ namespace Presentation.Controllers
 {
 	[ApiVersion("2.0")]
 	[ApiController]
-	[Route("api/v{version:apiVersion}/books")]// => localhost:46515/api/v2.0/books
+	[Route("api/books")]// => localhost:46515/api/v2.0/books
+	//[Route("api/v{version:apiVersion}/books")]// => localhost:46515/api/v2.0/books
 	public class BooksV2Controller : ControllerBase
 	{
 		private readonly IServiceManager _manager;
@@ -26,5 +27,8 @@ namespace Presentation.Controllers
 			var booksV2 = books.Select(b => new { Id = b.Id, Title = b.Title });
 			return Ok(booksV2);
 		}
+		//query string ile versiyonlama, {{baseUrl}}/api/books?api-version=2.0
+		//url ile versiyonlama yaptik,	{{baseUrl}}/api/v2.0/books
+		//simdi de header ile versiyonlama yapalim
 	}
 }
