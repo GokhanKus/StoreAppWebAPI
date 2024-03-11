d3b212055d8bdfcb5544f2762a61f73b42a80677
GokhanKus <gkus1998@hotmail.com>
2024-03-11 14:46:54 +0300

    IAuthService.cs AuthService.cs ve RegisterUser()

---
+++ Services/Concrete/AuthService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Entities.DTOs;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Services.Contracts;
+
+namespace Services.Concrete
+{
+	public class AuthService : IAuthService
+	{
+		private readonly UserManager<User> _userManager;
+		private readonly IMapper _mapper;
+		private readonly ILoggerService _logger;
+		//private readonly IConfiguration _configuration;
+		public AuthService(IMapper mapper, UserManager<User> userManager, ILoggerService logger)
+		{
+			_userManager = userManager;
+			_mapper = mapper;
+			_logger = logger;
+		}
+
+		public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto)
+		{
+			var user = _mapper.Map<User>(userForRegistrationDto);
+
+			var result = await _userManager.CreateAsync(user, userForRegistrationDto.Password);
+
+			if (result.Succeeded)
+				await _userManager.AddToRolesAsync(user, userForRegistrationDto.Roles);
+			
+			return result;
+		}
+	}
+}
