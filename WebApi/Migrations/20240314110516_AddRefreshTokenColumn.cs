dca3410185bea522c39173d1a21e465cfcb59a1e
GokhanKus <gkus1998@hotmail.com>
2024-03-14 14:07:56 +0300

    RefreshToken ve RefreshTokenExpiryTime proplar for user table

---
+++ WebApi/Migrations/20240314110516_AddRefreshTokenColumn.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "14b5dde9-f5b9-4c9f-8776-dfbd6cb00756");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "34efaaab-d7b1-4662-8c31-f54e3508ce36");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7e66a5c8-f339-4cc9-af86-74641029ef0f");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "0dda63b8-783e-4bef-b83d-9d9c64480b05", null, "User", "USER" },
+                    { "9daa292a-d4d0-4fa6-9ceb-28c99cacbcf5", null, "Admin", "ADMIN" },
+                    { "e8cfc050-c519-4161-8e88-15caaaa8310e", null, "Editor", "EDITOR" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedTime",
+                value: new DateTime(2024, 3, 14, 14, 5, 15, 495, DateTimeKind.Local).AddTicks(3568));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedTime",
+                value: new DateTime(2024, 3, 14, 14, 5, 15, 495, DateTimeKind.Local).AddTicks(3577));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedTime",
+                value: new DateTime(2024, 3, 14, 14, 5, 15, 495, DateTimeKind.Local).AddTicks(3579));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedTime",
+                value: new DateTime(2024, 3, 14, 14, 5, 15, 495, DateTimeKind.Local).AddTicks(3582));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0dda63b8-783e-4bef-b83d-9d9c64480b05");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9daa292a-d4d0-4fa6-9ceb-28c99cacbcf5");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e8cfc050-c519-4161-8e88-15caaaa8310e");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "14b5dde9-f5b9-4c9f-8776-dfbd6cb00756", null, "Editor", "EDITOR" },
+                    { "34efaaab-d7b1-4662-8c31-f54e3508ce36", null, "Admin", "ADMIN" },
+                    { "7e66a5c8-f339-4cc9-af86-74641029ef0f", null, "User", "USER" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedTime",
+                value: new DateTime(2024, 3, 11, 13, 56, 50, 942, DateTimeKind.Local).AddTicks(701));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedTime",
+                value: new DateTime(2024, 3, 11, 13, 56, 50, 942, DateTimeKind.Local).AddTicks(705));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedTime",
+                value: new DateTime(2024, 3, 11, 13, 56, 50, 942, DateTimeKind.Local).AddTicks(708));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedTime",
+                value: new DateTime(2024, 3, 11, 13, 56, 50, 942, DateTimeKind.Local).AddTicks(710));
+        }
+    }
+}
