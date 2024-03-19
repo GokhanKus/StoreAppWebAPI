66ade8d5564661b43d82c428f920c56c89648cdf
GokhanKus <gkus1998@hotmail.com>
2024-03-19 14:07:00 +0300

    categorydto class and CategoryNotFoundException.cs

---
+++ Entities/Exceptions/CategoryNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Exceptions
+{
+	public sealed class CategoryNotFoundException : NotFoundException //sealed oldugu kalıtım ile devralamazsin
+	{
+		public CategoryNotFoundException(int id) : base($"The category with id: {id} could not found")
+		{
+
+		}
+	}
+}
